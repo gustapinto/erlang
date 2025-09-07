64711d1526f5794fe3bef392cdface066f8ac93d
Gustavo Henrique Pinto <ghp2201@gmail.com>
2025-09-06 22:18:43 -0300

    feat: conditionals

---
+++ src/conditional.erl
@@ -0,0 +1,20 @@
+-module(conditional).
+-export([even/1, hello/1]).
+
+% Em Erlang ifs são chamados de guard patterns, e atuam com uma sintaxe similar
+% as guards, com uma cadeia de ifs sempre tendo que retornar um valor
+even(N) ->
+    if N rem 2 =:= 0 -> true;
+        true -> false  % "else"
+    end.
+
+% Ifs podem conter várias expressões e podem ser usados como valores
+hello(Animal) ->
+    LowerCaseAnimal = string:lowercase(Animal), % Usando o módulo padrão "string"
+    Message = if
+        LowerCaseAnimal =:= "cat" -> "Meow";
+        LowerCaseAnimal =:= "dog" -> "Oof";
+        true -> "<strange sound>"
+    end,
+
+    io:format("It says ~p!~n", [Message]).
---
+++ src/guard.erl
@@ -0,0 +1,13 @@
+-module(guard).
+-export([is_adult/1]).
+
+% Guards são uma forma de fluxo de controle, aplicadas usando a expressão "when"
+is_adult(Age) when Age >= 18 ->
+    true;
+
+is_adult(Age) when Age =< 0 ->
+    % Disparando um erro usando [error/1]
+    error("Invalid Age, must be at least >= 1");
+
+is_adult(_) ->
+    false.
